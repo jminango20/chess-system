0890a384ec758c3d83b894bdb5498d8ebac964cc
jminango20 <jcarlosminango@gmail.com>
2021-02-20 11:26:32 -0300

    Piece move account

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,7 +7,9 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
 	
 	private Color color;
+	private int moveCount;
 
+	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -17,6 +19,18 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount ++;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount --;
+	}
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 		
