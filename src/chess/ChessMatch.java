56ca928820d4c4e6463de1e4c9ef94e0113b3bb0
jminango20 <jcarlosminango@gmail.com>
2021-02-20 11:19:26 -0300

    Checkmate logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -16,6 +16,7 @@ public class ChessMatch { //coracao do Programa: regras do jogo
 	private Color currentPlayer;
 	private Board board; //Partida de xadrez tem que ter um tabuleiro
 	private boolean check;
+	private boolean checkMate;
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -38,6 +39,11 @@ public class ChessMatch { //coracao do Programa: regras do jogo
 	public boolean getCheck() {
 		return check;
 	}
+
+	public boolean getCheckMate() {
+		return checkMate;
+	}
+
 	
 	public ChessPiece[][] getPieces(){ //retorna uma matriz de pecas xadrez correspondente a partida
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -67,7 +73,12 @@ public class ChessMatch { //coracao do Programa: regras do jogo
 			throw new ChessException("You can not put yourself in check");
 		}
 		check = (testCheck(opponent(currentPlayer))) ? true:false;
-		nextTurn();
+		if(testCheck(opponent(currentPlayer))) {
+			checkMate = true;
+		}
+		else {
+			nextTurn();			
+		}
 		return (ChessPiece) capturedPiece;	
 	}
 	
@@ -145,6 +156,34 @@ public class ChessMatch { //coracao do Programa: regras do jogo
 	    }
 	    return false;
 	}
+
+
+	private boolean testCheckMate(Color color) {
+		if(!testCheck(color)) {
+			return false;
+		}
+		List<Piece> list = piecesOnTheBoard.stream().filter(x->((ChessPiece)x).getColor()==opponent(color)).collect(Collectors.toList()); 
+		for(Piece p:list) {
+			boolean[][] mat = p.possibleMoves();
+			for(int i=0;i<board.getRows(); i++) {
+				for(int j=0; j<board.getColumns(); j++) {
+					if(mat[i][j]) {
+						Position source = ((ChessPiece)p).getChessPosition().toPosition();
+						Position target = new Position(i,j);
+						Piece capturedPiece = makeMove(source, target);
+						boolean testCheck = testCheck(color);
+						undoMove(source, target, capturedPiece);
+						if(!testCheck) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	
 	
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
@@ -154,7 +193,7 @@ public class ChessMatch { //coracao do Programa: regras do jogo
 	
 	
 	private void initialSetup() {
-		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		/*placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
         placeNewPiece('d', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 2, new Rook(board, Color.WHITE));
@@ -167,6 +206,14 @@ public class ChessMatch { //coracao do Programa: regras do jogo
         placeNewPiece('e', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
+        */
+		placeNewPiece('h', 7, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('b', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 8, new King(board, Color.BLACK));
+
 	}
 
 }
